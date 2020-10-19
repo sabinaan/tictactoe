0280f1d0c16b8e1671bdbbb6c4720bfc0487e511
Sabina Andersson <sabinaan88@gmail.com>
2020-10-19 10:02:06 +0200

    Explained the project in README. Added a html skeleton and a css file. Added a js file with draft of modules and factory functions needed.

---
+++ README.md
@@ -0,0 +1,5 @@
+#tictactoe
+
+This is a tic tac toe game you can play in your browser.
+Made with HTML, CSS and Javascript. Using factory functions and the module pattern.
+Made as a part of the odin project.
---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+//gameboard 
+
+const gameBoard = (() => {
+    //gameBoardArray = new Array(9)
+    gameBoardArray = ["","","","","","","","",""];
+    return {gameBoardArray}
+})();
+
+//players
+
+const player = (name) => {
+    return {name}
+};
+
+// game
+
+const gameControll = (() => {
+    
+    return{}
+})();
